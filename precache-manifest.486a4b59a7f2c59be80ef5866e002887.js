74c920587ec2a046438c80a5d3577fca18af2741
unknown <souvikelric@gmail.com>
2020-06-15 18:37:16 +0530

    Updates

---
+++ precache-manifest.486a4b59a7f2c59be80ef5866e002887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad308c5fbb23eccd8682be3bc9ffcb7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c18d9f2071d220cd5565",
+    "url": "/robofriends/static/css/main.5b5ffc85.chunk.css"
+  },
+  {
+    "revision": "8ac66cf85c535b6f0889",
+    "url": "/robofriends/static/js/2.a8a83e8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a8a83e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18d9f2071d220cd5565",
+    "url": "/robofriends/static/js/main.481b423d.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
